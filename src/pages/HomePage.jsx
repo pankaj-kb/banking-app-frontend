0e5c71209f652291a8f337b3384f45cca178220f
pankaj-kb <pankajb2601@gmail.com>
2024-02-18 13:48:44 +0530

    adding home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import BankerHome from "../components/BankerHome";
+import CustomerHome from "../components/CustomerHome";
+
+const HomePage = () => {
+  return (
+    <div>
+      <CustomerHome />
+    </div>
+  );
+};
+
+export default HomePage;
