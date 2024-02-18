7eb2a794807769744ad0258bbc2006227865a938
pankaj-kb <pankajb2601@gmail.com>
2024-02-18 20:26:06 +0530

    adding userData and role dispatch to authRouter

--- src/AuthRouter.jsx
+++ src/AuthRouter.jsx
@@ -6,8 +6,29 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
 
 function AuthRouter() {
-    const dispatch = useDispatch();
-    
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await axios.get("/customer/current-user");
+        console.log(response);
+        if (response.data.statusCode === 200) {
+          dispatch(login(response.data.data));
+          if (!response.data.data?.role) {
+            dispatch(setRole("customer"));
+          } else {
+            dispatch(setRole("banker"))
+          }
+          navigate("/");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUser();
+  }, [dispatch]);
+
   return <RouterProvider router={router} />;
 }
 
