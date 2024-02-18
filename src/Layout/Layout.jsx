8f524ca287bfc88e86f95b94935e550ef3d3f732
pankaj-kb <pankajb2601@gmail.com>
2024-02-18 23:56:41 +0530

    updated layout styling

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -6,10 +6,12 @@ const Layout = () => {
   const loginStatus = useSelector((state) => state.auth.status);
   return loginStatus ? (
     <div className="flex min-h-screen bg-accentoffwhite">
-      <div className="flex justify-center items-center">
+      <div className="flex flex-col flex-1 overflow-y-auto">
+      <div className="sticky top-0 z-50 bg-accentoffwhite p-4 flex items-center justify-around">
         <NavBar />
       </div>
       <Outlet />
+      </div>
     </div>
   ) : (
     <Outlet />
