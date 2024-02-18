f413c4c6dd7eb572ae084df48a05eac3faa0bf4c
pankaj-kb <pankajb2601@gmail.com>
2024-02-18 13:01:24 +0530

    added logout button and styling to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,46 @@
-const NavBar = () => {
-    return (
-        <>
-        <h1>Bank Name</h1>
-        <h1>`Hello User`</h1>
-        <button>Logout</button>
-        </>
-    )
-}
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/authSlice";
+import Logo from "./Logo";
 
-export default NavBar
+const NavBar = () => {
+  const userData = useSelector((state) => state.auth.userData);
+  console.log(userData);
+  const userType = useSelector((state) => state.auth.role);
+  console.log(userType);
+
+  const dispatch = useDispatch();
+  // useEffect(() => {
+
+  // }, [])
+
+  const handleLogout = async () => {
+    const response = await axios.post(`/${userType}/logout`, {});
+    console.log(response);
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <Logo
+        className={
+          "absolute top-[5%] text-[50px] hover:cursor-pointer text-accentpurple font-semibold"
+        }
+      />
+      <div className="flex gap-[100px] items-center text-center">
+        <h1 className="font-medium text-[20px] text-accentpurple hover:cursor-pointer">
+          Hello, {userData.fullName}
+        </h1>
+        <button
+          className="p-2 font-medium text-accentwhite text-[20px] bg-accentpurple rounded-md"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
