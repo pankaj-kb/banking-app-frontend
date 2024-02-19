3dc275dcacd7687632f9a40fb6b73ebbc9eae0fc
pankaj-kb <pankajb2601@gmail.com>
2024-02-19 10:57:13 +0530

    trying differnt approach with axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { BASE_URL } from './api/api.js';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.withCredentials = true;
+axios.defaults.headers.common["Cookie"] = cookies.get("accessToken")
+// axios.defaults.headers.common["Cookie"] = cookies.get("refreshToken")
 
 export default axios;
