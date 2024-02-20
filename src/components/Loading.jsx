5cf8ee8072c4a888cd261ec6dd432949d95a417b
pankaj-kb <pankajb2601@gmail.com>
2024-02-20 17:56:07 +0530

    adding loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-page">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default Loading;
