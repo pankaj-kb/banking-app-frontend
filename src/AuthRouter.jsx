b5645f5b8b51300b8dcc6f3fe1279c2ef045428a
pankaj-kb <pankajb2601@gmail.com>
2024-02-20 17:56:37 +0530

    adding loading page to authrouter

--- src/AuthRouter.jsx
+++ src/AuthRouter.jsx
@@ -4,13 +4,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { login, setRole } from "./features/authSlice";
 import { RouterProvider, useNavigate } from "react-router-dom";
 import { router } from "./App";
+import Loading from "./components/Loading.jsx";
 
 function AuthRouter() {
   const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth.userData);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUser = async () => {
       try {
+        setLoading(true);
         const response = await axios.get("/customer/current-user");
         console.log(response);
         if (response.data.statusCode === 200) {
@@ -18,17 +22,22 @@ function AuthRouter() {
           if (!response.data.data?.role) {
             dispatch(setRole("customer"));
           } else {
-            dispatch(setRole("banker"))
+            dispatch(setRole("banker"));
           }
         }
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
     getUser();
   }, [dispatch]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <>{loading ? <Loading /> : <RouterProvider router={router} />}</>
+    // <><RouterProvider router={router}/></>
+  );
 }
 
 export default AuthRouter;
